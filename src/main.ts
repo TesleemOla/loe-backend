7870033b63fad94b9bc609b5d2ef06cede8c53b7
Tesleem Oladepo <oladepotesleem5@gmail.com>
2026-04-11 12:08:21 +0100

    added cors for frontend url

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const config = new ConfigService();
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: config.get<string>('FRONTEND_URL') || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(config.get<number>('PORT') ?? 3001);
 }
 bootstrap();
